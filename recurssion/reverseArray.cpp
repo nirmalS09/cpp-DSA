2290add9177474ff3f9f49730688b7f9771f9a89
Nirmal <nirmalswain028@gmail.com>
2026-03-05 18:35:52 +0530

    recurssion

--- recurssion/reverseArray.cpp
+++ recurssion/reverseArray.cpp
@@ -1,2 +1,36 @@
 #include<iostream>
 using namespace std;
+
+int swap(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void reverseArr(int arr[], int n){
+    int left = 0,right = n-1;
+    while(left<right){
+        swap(arr[left],arr[right]);
+        left++;right--;
+
+    }
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter the total number of elements"<<endl;
+    cin>>n;
+    int arr[n];
+
+    cout<<"Enter the elements: "<<endl;
+    for(int i =0; i<n;i++){
+        cin>>arr[i];
+    }
+    reverseArr(arr,n);
+    for(int i =0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+}
