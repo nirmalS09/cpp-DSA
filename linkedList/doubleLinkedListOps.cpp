e768bd7302fbbc7d8d70510e7a06bc8b8db22c25
Nirmal <nirmalswain028@gmail.com>
2026-06-15 01:00:27 +0530

    doubleLinkedList_2

--- linkedList/doubleLinkedListOps.cpp
+++ linkedList/doubleLinkedListOps.cpp
@@ -12,12 +12,13 @@ class Node{
             back = back1;
         }
         Node(int data1){
-            data = data;
+            data = data1;
             next = nullptr;
             back = nullptr;
         }
 };
 Node *converArrToDL(vector<int> &arr){
+    if(arr.empty()) return nullptr;
     Node *head = new Node(arr[0]);
     Node *prev = head;
     for(int i = 1; i<arr.size();i++){
@@ -35,8 +36,40 @@ void printDLL(Node *head){
     }
     cout<<endl;
 }
+Node *deleteHead(Node *head){
+    if(head==NULL) return NULL;
+    if(head->next==NULL){
+        delete head;
+        return nullptr;
+    }
+    Node *temp = head;
+    head = head->next;
+    head->back = nullptr;
+    temp->next = nullptr;
+    delete temp;
+    return head;
+}
+Node *deleteEnd(Node *head){
+    if(head==NULL) return head;
+    if(head->next==NULL){
+        delete head;
+        return nullptr;
+    }
+    Node *temp = head;
+    while(temp->next->next) temp = temp->next;
+    delete temp->next;
+    temp->next = nullptr;
+    return head;
+}
+Node *deleteKTHelement(Node *head){
+    
+}
 int main(){
-    vector<int> arr={4,2,6,5,9,1,7,8};
+    vector<int> arr={4,3,6,2,8,6};
     Node *head = converArrToDL(arr);
+
+    printDLL(head);
+    head = deleteHead(head);
+    // head = deleteEnd(head);
     printDLL(head);
 }
